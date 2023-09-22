34d9bd85021cb69ee72ee32e91de6d0337f4e968
Valerie Yauw <valerie.yauw@gmail.com>
2023-09-22 14:51:31 +0700

    Design API entrypoints

--- express-app/__tests__/entrypoints.js
+++ express-app/__tests__/entrypoints.js
@@ -1,29 +1,42 @@
 const entrypoints = {
-
+  
+  // POST
   login: '/login',
   register: '/register',
 
+  // GET
   users: (pageNumber = 1) => `/users?p=${pageNumber}`,
   user: (id) => `/users/${id}`,
 
+  // GET (my own details)
   me: '/user',
   myReviews: '/user/reviews',
   myJobApplications: '/user/job-applications',
   myJobPostings: '/user/job-postings',
 
-  // TODO: Testing
-  jobPostings: (pageNumber = 1, {requiredGender, maxAge, minEducation, location, isUrgent} = {}) => {
+  // GET
+  jobPostings: ({pageNumber = 1, requiredGender, maxAge, categoryId, education, location, isUrgent} = {}) => {
     let query = '';
     if (requiredGender) query += `&gender=${requiredGender}`;
     if (maxAge) query += `&maxAge=${maxAge}`;
-    if (minEducation) query += `&minEducation=${minEducation}`;
+    if (categoryId) query += `&categoryId=${categoryId}`
+    if (education) query += `&education=${education}`;
     if (location) query += `&location=${location}`;
     if (isUrgent) query += `&isUrgent=${isUrgent}`;
     return `/job-postings?p=${pageNumber}${query}`
   },
   jobPosting: (id) => `/job-postings/${id}`,
+  
+  // POST
+  addJobPosting: '/job-postings',
+  
+  // PUT
+  editJobPosting: (id) => `/job-posting/${id}`,
 
-  applyToJob: (id) => `/job-postings/${id}/apply`,
+  // POST, PATCH
+  applyToJob: (id) => `/job-postings/${id}/application`,
+
+  // POST
   reviewUser: (id) => `/users/${id}/review`,
 
 }
