667ac3c1e75cef5af75655b966ffda0e729b5797
Valerie Yauw <valerie.yauw@gmail.com>
2023-09-23 11:14:20 +0700

    Seeders

---
+++ express-app/seeders/20230923032715-job-application.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const rawSeedData = JSON.parse(fs.readFileSync('../data/job-applications.json'));
+    const seedData = rawSeedData.map(el => {
+      const createdAt = new Date();
+      const updatedAt = new Date();
+      return { ...el, createdAt, updatedAt }
+    });
+    await queryInterface.bulkInsert('JobApplications', seedData, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('JobApplications', null, {
+      restartIdentity: true,
+      cascade: true,
+      truncate: true
+    });
+  }
+};
